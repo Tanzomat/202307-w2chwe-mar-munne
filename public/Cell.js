99a112b2bb27fdb550c8d605fd262c93e55336fa
Mar Munné <mar.munnesteve@gmail.com>
2023-07-23 22:55:37 +0200

    Add getAliveNeighbors method w/ position

--- public/Cell.js
+++ public/Cell.js
@@ -6,6 +6,19 @@ class Cell {
     this.position = position;
     this.isAlive = isAlive;
   }
+
+  getAliveNeighbors(board) {
+    this.neighbors =
+      board[this.position[0] - 1][this.position[1] - 1] +
+      board[this.position[0] - 1][this.position[1] + 0] +
+      board[this.position[0] - 1][this.position[1] + 1] +
+      board[this.position[0] + 0][this.position[1] - 1] +
+      board[this.position[0] + 0][this.position[1] + 0] +
+      board[this.position[0] + 0][this.position[1] + 1] +
+      board[this.position[0] + 1][this.position[1] - 1] +
+      board[this.position[0] + 1][this.position[1] + 0] +
+      board[this.position[0] + 1][this.position[1] + 1];
+  }
 }
 
 export default Cell;

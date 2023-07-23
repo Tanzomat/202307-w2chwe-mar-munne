b31f88316f852163633b607be35bb658ac7e9058
Mar Munné <mar.munnesteve@gmail.com>
2023-07-23 23:25:50 +0200

    Implement 1st rule to getNewCellState fn

--- public/Cell.js
+++ public/Cell.js
@@ -19,6 +19,11 @@ class Cell {
       board[this.position[0] + 1][this.position[1] + 0] +
       board[this.position[0] + 1][this.position[1] + 1];
   }
+
+  getNewCellState() {
+    const neighbors = this.getAliveNeighbors();
+    if (this.isAlive && neighbors > 3) return true;
+  }
 }
 
 export default Cell;
